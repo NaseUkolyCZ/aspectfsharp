c620253f941adfb61e2329855c1eff8738679917
David Podhola <david@podhola.net>
2016-01-18 22:35:52 +0100

    devkit init & install added

--- setup.bat
+++ setup.bat
@@ -1,5 +1,10 @@
 choco install ruby -y
 choco install ruby2.devkit -y
+set mypath=%cd%
+cd C:\tools\DevKit2
+ruby dk.rb init
+ruby dk.rb install
+cd %mypath%
 call gem install bundler
 rem gem install jekyll
 bundle install
